d53c3ace3cc1f8f05d2a419d42b99e1ffc9509aa
nahueldefazio <nahuel1116@gmail.com>
2021-09-06 16:16:52 -0300

    2da Entrega
    Agrego el componente NavBar con su .js y .css; a su vez utilizando la biblioteca de Boostrap. Cambie el nombre del "Title" y cambie el favicon.ico. Faltaria ver la paleta de colores y fonts, lo cual lo veré mas adelante

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,44 @@
+import React, {Fragment} from 'react';
+import logo from '../../images/muebles.png'
+import '../NavBar/NavBar.css'
+
+
+function NavBar(props) {
+    return (
+        <Fragment>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#"><img src={logo}/></a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false"
+                            aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse justify-content-end fontSize" id="navbarNavDropdown">
+                        <ul className="navbar-nav subMenus">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Inicio</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Muebles Exterior</a>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink"
+                                   role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Muebles Interior
+                                </a>
+                                <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                    <li><a className="dropdown-item" href="#">Living</a></li>
+                                    <li><a className="dropdown-item" href="#">Cocina</a></li>
+                                    <li><a className="dropdown-item" href="#">Dormitorio</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </Fragment>
+    );
+}
+
+export default NavBar;
