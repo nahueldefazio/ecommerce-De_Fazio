d660cbd6ff2789023c5a8bffc90207fc7ac47742
nahueldefazio <nahuel1116@gmail.com>
2021-09-20 03:18:54 -0300

    Update components

--- src/Components/Item/Item.js
+++ src/Components/Item/Item.js
@@ -1,13 +1,15 @@
 import React, {Fragment} from 'react';
 import ItemCount from "../ItemCount/ItemCount";
 import './Item.css'
+import imagen from '../../images/muebles.png'
 
 function Item(props) {
     //{titulo: "", precio:""
     return (
         <Fragment>
             <div className="card " style={{width: "18rem"}}>
-                <img className="card-img-top" src="..." alt="Card image cap"/>
+                {/* eslint-disable-next-line jsx-a11y/img-redundant-alt */}
+                <img className="card-img-top" src={"..."} alt="Card image cap"/>
                     <div className="card-body">
                         <h5 className="card-title">Card title</h5>
                         <p className="card-text">Some quick example text to build on the card title and make up the bulk
